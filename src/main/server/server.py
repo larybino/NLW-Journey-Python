ac9b160edac9778532b105d714c9e039e62f04c2
larybino <binolaryssa@gmail.com>
2024-07-12 15:39:24 -0300

    finalizando projeto

--- src/main/server/server.py
+++ src/main/server/server.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from src.main.routes.trips_routes import trips_routes_bp
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 app.register_blueprint(trips_routes_bp)
+if __name__ == '__main__':
+    app.run(debug=True)
